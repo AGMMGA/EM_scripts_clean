bdc2bdb967846d94792030af8e88fa26fd42b826
Andrea Murachelli <andrea.murachelli@gmail.com>
2018-07-20 17:06:41 +0200

    whatever

--- word_frequency_psiblast.py
+++ word_frequency_psiblast.py
@@ -4,6 +4,33 @@ import numpy as np
 import re
 from bs4 import BeautifulSoup
 
+clean = ['\[.*\]',
+             'isoform X[0-9]',
+             'transcript variant X[0-9]',
+             'Full=',
+             'AltName:',
+             'RecName:',
+             'isoform [A-Za-z]',
+             'partial',
+             'isoform [0-9]',
+             '[A-Z0-9]+_[A-Za-z0-9]+',
+             ]
+
+useless_words = {'[uU]ncharacteri[sz]ed',
+                     '[Pp]rotein',
+                     'PROTEIN',
+                     'LOW'
+                     'QUALITY',
+                     'containing',
+                     'domain-containing',
+                     '[Cc]onserved',
+                     '[Dd]omain',
+                     '[Hh]ypothetical',
+                     'family',
+                     'motif',
+                     'function',
+                     'unknown'}  #shorter than 2
+
 def plot_frequencies(final_text):
     freq={}
 #     match_pattern = re.findall(r'\b[a-z]{1,15}\b', final_text)
@@ -18,8 +45,8 @@ def plot_frequencies(final_text):
         count=freq.get(word,0)
         freq[word]=count + 1
     frequency_list = freq.keys()
-    for words in frequency_list:
-        print(words + ' -> ' + str(freq[words]))
+#     for words in frequency_list:
+#         print(words + ' -> ' + str(freq[words]))
     results = []
     for word in frequency_list:
         tuple_ = (word, freq[word])
@@ -28,7 +55,7 @@ def plot_frequencies(final_text):
     words_names=[]
     words_count=[]
     for (word, freq) in byFreq[:]:
-        print (word, freq)
+#         print (word, freq)
         words_names.append(word)
         words_count.append(freq)
     # Plot histogram using matplotlib bar()
@@ -48,34 +75,33 @@ def parse_result(xml_file):
         soup = BeautifulSoup(f.read(), 'lxml')
     descriptions = soup.find_all('hit_def')
     tags = []
+    mega_regexp = '|'.join(clean)
     for text in descriptions:
         for tag in text:
-            clean = ['\[.*\]',
-                     'isoform X[0-9]',
-                     'transcript variant X[0-9]',
-                     'Full=',
-                     'AltName:',
-                     'RecName:',
-                     'isoform [A-Za-z]',
-                     'partial',
-                     'isoform [0-9]',
-                     '[A-Z0-9]+_[A-Za-z0-9]+'
-                     ]
-            t = tag
-            for pattern in clean: 
-                t = re.sub(pattern, '', t) #remove species names
-            tags.append(t) 
-#     tags = list(set(tags))
-    words = ''.join(tags)
-    return tags, words
+            t = re.sub(mega_regexp, '', tag) #remove species names and other bullshit
+            tags.append(t)
+    mega_regexp = re.compile('|'.join(useless_words))
+    words = []
+    discarded = []
+    for tag in tags[:]:
+        for token in tag.split():
+            token = re.sub('[\(\[\{\}\]\)\,\;\.\:]','',token) #remove punctuation & parentheses
+            t = re.sub('[^A-z0-9]', '', token) #otherwise N-formyl will match
+            if re.compile(r'\b[A-z]{1,2}\b').match(t): 
+                continue
+            if not mega_regexp.search(token):
+                words.append(token)
+            else:
+                discarded.append(token)
+    return tags, ' '.join(words)
             
     
 
 def main(xml_file):
     tags, words = parse_result(xml_file)
     x = plot_frequencies(words)
-    y = plot_frequencies(tags)
-    plt.show()
+#     y = plot_frequencies(tags)
+#     plt.show()
 
 
     
